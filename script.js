7d2913982d948ac48af362253b982261a8110047
Open Source Coding <116367908+opensource-coding@users.noreply.github.com>
2023-05-07 17:32:34 +0500

    Final

---
+++ script.js
@@ -0,0 +1,10 @@
+$(document).ready(function () {
+  // move underline
+  // Mobile menu
+  // Add sticky class if scrolled
+  // skills round circle svg
+  //Portfolio gallery
+  // FORM VALIDATION
+  // THEME PRIMARY COLOR SETTINGS
+  // DARK MODE
+});
